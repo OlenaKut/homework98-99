920312cbb6f45d17de3578ad6180f2e6bb3f7fd9
OlenaKut <cundrochka@gmail.com>
2022-11-03 14:25:33 +0100

    Added name

--- src/Users.js
+++ src/Users.js
@@ -3,7 +3,7 @@ function Users(props) {
   return (
     <div>
       <h3>Hello </h3>
-      <h1>{props.name}</h1>
+      <h1>I'm {props.name}</h1>
       <img src={props.avatar} alt="User" className="rounded-3 m-3" />
     </div>
   );
